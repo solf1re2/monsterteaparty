67efee69f85f16c446bbe6ef3c43470af5b23367
James <jy1v07@gmail.com>
2016-09-17 19:06:53 +0100

    expanded commandList

--- command/commandlist.go
+++ command/commandlist.go
@@ -5,13 +5,31 @@ func ProcessCommandInput(input string) {
     switch input {
     case north:
         //move player up one tile
+        fmt.Println("player move north\n")
+        movePlayer(0, 1)
     case south:
         //move player down one tile
+        fmt.Println("player move south\n")
+        movePlayer(0,-1)
     case east:
         //move player right one tile
+        fmt.Println("player move east\n")
+        movePlayer(1,0)
     case west:
         //move player left one tile
-    default:
+        fmt.Println("player move west\n")
+        movePlayer(-1,0)
 
+
+
+
+
+
+    default:
+        fmt.Println("default exec\n")
     }
+}
+
+func movePlayer(changeX int, changeY int) {
+
 }
