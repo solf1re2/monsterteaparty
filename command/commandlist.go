1db609d2b26d6212e743d0fd35474dcf3d1d81c8
James <jy1v07@gmail.com>
2016-09-20 11:34:07 +0100

    fixed command list switch to strings

--- command/commandlist.go
+++ command/commandlist.go
@@ -1,33 +1,32 @@
 package command
 
+import (
+	"fmt"
+)
 
+//ProcessCommandInput - command sorting.
 func ProcessCommandInput(input string) {
-    switch input {
-    case north:
-        //move player up one tile
-        fmt.Println("player move north\n")
-        movePlayer(0, 1)
-    case south:
-        //move player down one tile
-        fmt.Println("player move south\n")
-        movePlayer(0,-1)
-    case east:
-        //move player right one tile
-        fmt.Println("player move east\n")
-        movePlayer(1,0)
-    case west:
-        //move player left one tile
-        fmt.Println("player move west\n")
-        movePlayer(-1,0)
+	switch input {
+	case "north":
+		//move player up one tile
+		fmt.Println("player move north\n")
+		movePlayer(0, 1)
+	case "south":
+		//move player down one tile
+		fmt.Println("player move south\n")
+		movePlayer(0, -1)
+	case "east":
+		//move player right one tile
+		fmt.Println("player move east\n")
+		movePlayer(1, 0)
+	case "west":
+		//move player left one tile
+		fmt.Println("player move west\n")
+		movePlayer(-1, 0)
 
-
-
-
-
-
-    default:
-        fmt.Println("default exec\n")
-    }
+	default:
+		fmt.Println("default exec\n")
+	}
 }
 
 func movePlayer(changeX int, changeY int) {
